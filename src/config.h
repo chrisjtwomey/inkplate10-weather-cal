1de693b9dcec9898480946d9847ea68204b3fc1e
Chris Twomey <chrisjamestwomey@gmail.com>
2022-10-31 21:55:02 +0000

    Images are downloaded and displayed to/from SD card

--- src/config.h
+++ src/config.h
@@ -2,10 +2,12 @@
 #include <MqttLogger.h>
 #include <NTPClient.h>
 #include <PubSubClient.h>
+#include <SdFat.h>
 #include <TimeLib.h>
 #include <WiFi.h>
 #include <WiFiUdp.h>
 #include <driver/rtc_io.h>
+#include <esp_adc_cal.h>
 #include <rom/rtc.h>
 
 #define WIFI_SSID "XXXX"  // replace with your WiFi SSID
@@ -32,15 +34,9 @@
 #define NTP_HOST "europe.pool.ntp.org"
 #endif
 #ifndef GMT_OFFSET
-#define GMT_OFFSET 1  // +X timezone (eg. GMT+1)
+#define GMT_OFFSET 0  // +X timezone (eg. GMT+1)
 #endif
 #define FALLBACK_SLEEP_SECONDS 120  // seconds to sleep if RTC not configured
-
-WiFiUDP udpClient;
-NTPClient timeClient(udpClient, NTP_HOST, GMT_OFFSET * 60 * 60, 60000);
-
-// inkplate10 board driver
-Inkplate display(INKPLATE_3BIT);
 
 #ifndef MQTT_BROKER
 #define MQTT_BROKER "localhost"
@@ -52,9 +48,6 @@ Inkplate display(INKPLATE_3BIT);
 #define MQTT_CLIENT_ID "eink-cal-client"
 #endif
 #define MQTT_TOPIC "mqtt/eink-cal-client"
-WiFiClient wifiClient;
-PubSubClient mqttClient(wifiClient);
-MqttLogger mqttLogger(mqttClient, MQTT_TOPIC, MqttLoggerMode::MqttAndSerial);
 
 #define LOG_CRIT 0
 #define LOG_ERROR 1
@@ -62,9 +55,20 @@ MqttLogger mqttLogger(mqttClient, MQTT_TOPIC, MqttLoggerMode::MqttAndSerial);
 #define LOG_NOTICE 3
 #define LOG_INFO 4
 #define LOG_DEBUG 5
+
 #ifndef LOG_LEVEL
 #define LOG_LEVEL LOG_INFO
 #endif
+
+// inkplate10 board driver
+Inkplate display(INKPLATE_3BIT);
+
+WiFiUDP udpClient;
+NTPClient timeClient(udpClient, NTP_HOST, GMT_OFFSET * 60 * 60, 60000);
+
+WiFiClient wifiClient;
+PubSubClient mqttClient(wifiClient);
+MqttLogger mqttLogger(mqttClient, MQTT_TOPIC, MqttLoggerMode::MqttAndSerial);
 
 const char* fmtTime(uint32_t t) {
     char* tstr = new char[20];
@@ -106,6 +110,7 @@ const char* msgPrefix(uint16_t pri) {
     return prefix;
 }
 
+// log formats a string with priority to send to Serial and MQTT logger
 void log(uint16_t pri, const char* msg) {
     if (pri > LOG_LEVEL) return;
 
@@ -119,6 +124,7 @@ void log(uint16_t pri, const char* msg) {
     mqttLogger.println(buf);
 }
 
+// logf formats a string with priority to send to Serial and MQTT logger
 void logf(uint16_t pri, const char* fmt, ...) {
     if (pri > LOG_LEVEL) return;
 
@@ -138,10 +144,50 @@ void logf(uint16_t pri, const char* fmt, ...) {
     va_end(args);
 }
 
+// getCalibratedBatteryVoltage returns the battery voltage by a calibrated ADC
+float getCalibratedBatteryVoltage() {
+    // calibration factor - adjust until correlates with actual readings
+    float calibration = 2.140;
+    // default voltage reference for esp32
+    float vref = 1100;
+
+    // get characterized voltage reference
+    esp_adc_cal_characteristics_t adc_chars;
+    esp_adc_cal_characterize(ADC_UNIT_1, ADC_ATTEN_DB_11, ADC_WIDTH_BIT_12,
+                             vref, &adc_chars);
+    vref = adc_chars.vref;
+
+    uint8_t mcpRegsInt[22];
+    display.pinModeInternal(MCP23017_INT_ADDR, mcpRegsInt, 9, INPUT);
+    int state = display.digitalReadInternal(MCP23017_INT_ADDR, mcpRegsInt, 9);
+    display.pinModeInternal(MCP23017_INT_ADDR, mcpRegsInt, 9, OUTPUT);
+
+    if (state) {
+        display.digitalWriteInternal(MCP23017_INT_ADDR, mcpRegsInt, 9, LOW);
+    } else {
+        display.digitalWriteInternal(MCP23017_INT_ADDR, mcpRegsInt, 9, HIGH);
+    }
+
+    delay(1);
+    int adc = analogRead(35);
+    if (state) {
+        display.pinModeInternal(MCP23017_INT_ADDR, mcpRegsInt, 9, INPUT);
+    } else {
+        display.digitalWriteInternal(MCP23017_INT_ADDR, mcpRegsInt, 9, LOW);
+    }
+
+    return (adc / 4095.0) * 3.3 * (1100 / vref) * calibration;
+}
+
+// isVbusPresent returns whether the board is being powered by USB
+bool isVbusPresent() {
+    // TODO: determine USB power?
+    return false;
+}
+
 void setup();
 void sleep();
-bool isVbusPresent();
 time_t getWakeTime();
-float getBatteryVoltage();
-void displayImage(const char* host, const int port, const char* path, int16_t x,
-                  int16_t y);
+void displayError(const char* msg);
+void displayImage(const char* filePath);
+void downloadImage(const char* url, const char* filePath);
--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,4 @@
 #define LOG_LEVEL LOG_DEBUG
-#define USE_DISPLAY false
 #include "config.h"
 
 RTC_DATA_ATTR int bootCount = 0;
@@ -11,9 +10,10 @@ RTC_DATA_ATTR unsigned long driftSecs = 0;
 void setup() {
     ++bootCount;
     Serial.begin(115200);
-    // init inkplate board
+    // Init inkplate board
     display.begin();
-    display.setTextSize(4);
+    // Set display to portait mode
+    display.setRotation(1);
 
     time_t bootTime = display.rtcGetEpoch();
     logf(LOG_INFO, "boot count: %d", bootCount);
@@ -42,41 +42,63 @@ void setup() {
             break;
     }
 
+    // Init storage
+    if (!display.sdCardInit()) {
+        const char* errMsg = "SD card error";
+        log(LOG_ERROR, errMsg);
+        displayError(errMsg);
+        sleep();
+    }
+    // If previous image exists, load into display buffer
+    if (!display.drawImage(IMAGE_HOST_PATH, 0, 0, false, true)) {
+        log(LOG_WARNING, "load previous calendar error");
+    }
+
+    // Get battery voltages before connecting to WiFi
+    float bvoltCal = getCalibratedBatteryVoltage();
+    // Get inkplate's calculation for reference
+    float bvolt = display.readBattery();
+
+    // Connect to WiFi
     WiFi.mode(WIFI_STA);
     WiFi.begin(WIFI_SSID, WIFI_PASS);
     logf(LOG_INFO, "connecting to WiFi SSID %s...", WIFI_SSID);
+
+    // Retry until success or give up
     int attempts = 0;
     while (attempts++ <= MAX_RETRIES && WiFi.status() != WL_CONNECTED) {
         delay(1000);
     }
+
+    // If give up, display error to display
     if (WiFi.status() != WL_CONNECTED) {
-        const char* errMsg = "WiFi connect timeout.";
+        const char* errMsg = "WiFi connect timeout";
         log(LOG_ERROR, errMsg);
-        display.print(errMsg);
-        if (USE_DISPLAY) display.display();
+        displayError(errMsg);
         sleep();
     }
     // Print the IP address
     logf(LOG_INFO, "IP address: %s", WiFi.localIP().toString());
 
-    // set up remote logging with mqtt broker
+    // Set up remote logging with mqtt broker
     logf(LOG_INFO, "configuring remote logging...");
     mqttClient.setServer(MQTT_BROKER, MQTT_PORT);
     attempts = 0;
     while (!mqttClient.connect(MQTT_CLIENT_ID)) {
         if (attempts++ >= MAX_RETRIES) {
-            log(LOG_WARNING, "failed to connect remote logging, fallback to serial");
+            log(LOG_WARNING,
+                "failed to connect remote logging, fallback to serial");
             break;
         }
         logf(LOG_WARNING, "mqtt connection failed, rc=%d", mqttClient.state());
         delay(250);
     }
 
-    // configure time
+    // Get time from NTP server
     timeClient.begin();
     timeClient.update();
+    // Sync RTC with NTP time
     display.rtcSetEpoch(timeClient.getEpochTime());
-
     logf(LOG_DEBUG, "RTC synced to %s", fmtTime(display.rtcGetEpoch()));
 
     if (lastBootTime > 0) {
@@ -97,8 +119,9 @@ void setup() {
         logf(LOG_INFO, "time drift: %d seconds", driftSecs);
     }
 
-    float bvolt = getBatteryVoltage();
-    logf(LOG_INFO, "battery voltage: %sv", String(bvolt, 2));
+    logf(LOG_INFO, "battery voltage: %sv", String(bvoltCal, 2));
+    logf(LOG_DEBUG, "reference inkplate battery voltage: %sv",
+         String(bvolt, 2));
 
     if (isVbusPresent()) {
         const char* bstat = (bvolt < 4.0) ? "charging" : "charged";
@@ -106,6 +129,7 @@ void setup() {
     } else if (bvolt > 0.0) {
         if (bvolt < 3.1) {
             log(LOG_NOTICE, "battery near empty! - sleeping until charged");
+            displayError("Battery empty, please charge!");
             sleep();
         } else if (bvolt < 3.3) {
             log(LOG_WARNING, "battery low, charge soon!");
@@ -117,11 +141,16 @@ void setup() {
         log(LOG_WARNING, "problem detecting battery voltage");
     }
 
-    displayImage(IMAGE_HOST, IMAGE_HOST_PORT, IMAGE_HOST_PATH, 0, 0);
+    // Format url
+    char url[256];
+    sprintf(url, "http://%s:%d%s", IMAGE_HOST, IMAGE_HOST_PORT,
+            IMAGE_HOST_PATH);
+
+    downloadImage(url, IMAGE_HOST_PATH);
+    displayImage(IMAGE_HOST_PATH);
     sleep();
 }
 
-// getWakeTime returns a time object representing the wake time
 time_t getWakeTime() {
     if (!display.rtcIsSet()) {
         log(LOG_WARNING, "cannot determine wake time: RTC not set");
@@ -142,7 +171,7 @@ time_t getWakeTime() {
     time_t targetTime = makeTime(tm);
     time_t nowTime = display.rtcGetEpoch();
 
-    // rollover to tomorrow
+    // Rollover to tomorrow
     if (nowTime > targetTime) {
         targetTime += SECS_PER_DAY;
     }
@@ -156,24 +185,55 @@ void sleep() {
     log(LOG_NOTICE, "deep sleep initiated");
     logf(LOG_DEBUG, "RTC time now is %s", fmtTime(display.rtcGetEpoch()));
     logf(LOG_DEBUG, "setting deep sleep RTC wakeup on pin %d", GPIO_NUM_39);
+
     display.rtcSetAlarmEpoch(targetWakeTime, RTC_ALARM_MATCH_DHHMMSS);
     esp_sleep_enable_ext0_wakeup(GPIO_NUM_39, 0);
+
     logf(LOG_INFO, "waking at %s", fmtTime(targetWakeTime));
     log(LOG_NOTICE, "deep sleeping in 5 seconds");
     delay(5000);
+
     WiFi.mode(WIFI_OFF);
     lastSleepTime = display.rtcGetEpoch();
     esp_deep_sleep_start();
 }
 
-void displayImage(const char* host, const int port, const char* path, int16_t x,
-                  int16_t y) {
-    char url[256];
-    sprintf(url, "http://%s:%d%s", host, port, path);
-    logf(LOG_INFO, "drawing image from URL: %s", url);
+void downloadImage(const char* url, const char* filePath) {
+    logf(LOG_INFO, "downloading image at URL %s", url);
 
-    if (USE_DISPLAY) display.clearDisplay();
-    if (USE_DISPLAY) display.display();
+    // Guestimate file size for PNG image @ 1200x825
+    int32_t len = E_INK_WIDTH * E_INK_HEIGHT * 4 + 100;
+    // Download file from URL
+    uint8_t* buf = display.downloadFile(url, &len);
+    if (!buf) {
+        const char* errMsg = "image download error";
+        log(LOG_ERROR, errMsg);
+        displayError(errMsg);
+        sleep();
+    }
+
+    logf(LOG_INFO, "writing image data to path %s", filePath);
+    SdFat sd = display.getSdFat();
+
+    // Write image buffer to SD card
+    if (sd.exists(filePath)) {
+        sd.remove(filePath);
+    }
+
+    File sdfile = sd.open(filePath, FILE_WRITE);
+    if (!sdfile) {
+        const char* errMsg = "file write error";
+        log(LOG_ERROR, errMsg);
+        displayError(errMsg);
+        sleep();
+    }
+
+    sdfile.write(buf, len);
+    sdfile.close();
+}
+
+void displayImage(const char* filePath) {
+    logf(LOG_INFO, "drawing image from path: %s", filePath);
 
     bool ok = false;
     int attempts = 0;
@@ -184,20 +244,27 @@ void displayImage(const char* host, const int port, const char* path, int16_t x,
             delay(5000);
         }
 
-        if (!display.drawImage(url, 0, 0, false, true)) {
-            log(LOG_ERROR, "image open error");
-            display.println("Image open error");
-            if (USE_DISPLAY) display.display();
+        display.clearDisplay();
+        if (!display.drawImage(filePath, 0, 0, false, true)) {
+            log(LOG_ERROR, "image draw error");
             continue;
         }
-        if (USE_DISPLAY) display.display();
+        display.display();
 
         logf(LOG_INFO, "draw attempt #%d success", attempts);
         ok = true;
     }
+
+    if (!ok) displayError("Calendar refresh failed");
 }
 
-float getBatteryVoltage() { return display.readBattery(); }
-bool isVbusPresent() { return false; }
+void displayError(const char* msg) {
+    display.setTextSize(4);
+    display.setTextColor(1, 0);
+    display.setTextWrap(true);
+    display.setCursor(0, 0);
+    display.print(msg);
+    display.display();
+}
 
 void loop() {}
