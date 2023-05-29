eba9bf886c272b7a32272c916892845aeb189686
Philip Dodd <philip@dodd.fr>
2023-05-29 12:50:38 +0200

    Improve the quick copy script to make a copy for arduino compilation

--- createArduino.ps1
+++
@@ -1,9 +0,0 @@
-# Arduino expects Sketches to be in a folder with the name of the sketch file, and the sketch file needs to have the .ino extension
-# This quickly copies the firmware source files and renames main.cpp to achieve this.
-
-Write-Host -NoNewline "Copying src directory to main..."
-Copy-Item -Path .\src -Destination .\main -Recurse
-Write-Host "done!"
-Write-Host -NoNewline "Renaming main/main.cpp to main/main.ino..."
-Move-Item -Path ./main/main.cpp -Destination ./main/main.ino 
-Write-Host "done!"
